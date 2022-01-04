6bf1474ad4ab74c6a625dafd7cc7226eaae6bbca
Daniel <danielcrewe1999@gmail.com>
2022-01-04 00:27:56 +0000

    work to bot

--- deploy_commands.js
+++ deploy_commands.js
@@ -6,13 +6,44 @@ import CONFIG from "./config_variables.js";
 const commands = [
   new SlashCommandBuilder()
     .setName("createpoll")
-    .setDescription("Creates a new poll"),
+    .setDescription("Creates a new poll")
+    .addStringOption((option) =>
+      option.setName("option_1").setDescription("Title for option 1")
+    )
+    .addStringOption((option) =>
+      option.setName("option_2").setDescription("Title for option 2")
+    )
+    .addStringOption((option) =>
+      option.setName("option_3").setDescription("Title for option 3")
+    )
+    .addStringOption((option) =>
+      option.setName("option_4").setDescription("Title for option 4")
+    )
+    .addStringOption((option) =>
+      option.setName("option_5").setDescription("Title for option 5")
+    )
+    .addStringOption((option) =>
+      option.setName("option_6").setDescription("Title for option 6")
+    )
+    .addStringOption((option) =>
+      option.setName("option_7").setDescription("Title for option 7")
+    )
+    .addStringOption((option) =>
+      option.setName("option_8").setDescription("Title for option 8")
+    )
+    .addStringOption((option) =>
+      option.setName("option_9").setDescription("Title for option 9")
+    )
+    .addStringOption((option) =>
+      option.setName("option_10").setDescription("Title for option 10")
+    ),
   new SlashCommandBuilder()
     .setName("closepoll")
-    .setDescription("Closes the current poll"),
+    .setDescription("Closes the running poll"),
   new SlashCommandBuilder()
-    .setName("vote")
-    .setDescription("Votes In Current Poll"),
+    .setName("viewpoll")
+    .setDescription("Views the current poll"),
+  new SlashCommandBuilder().setName("vote").setDescription("Votes in poll"),
 ];
 
 const rest = new REST({ version: "9" }).setToken(CONFIG.DISCORD_TOKEN);
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import CONFIG from "./config_variables.js";
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+let current_poll = [];
+
 // On Ready
 client.once("ready", () => {
   console.log(`=== Logged In As: ${client.user.tag} ===`);
@@ -15,12 +17,66 @@ client.on("error", (error) => {
   console.log(error);
 });
 
+// On Command Usage
 client.on("interactionCreate", async (interaction) => {
   if (interaction.isCommand()) {
     const { commandName } = interaction;
 
-    if (commandName === "ping") {
-      await interaction.reply("Pong!");
+    if (interaction.user.id.toString() === CONFIG.OWNER) {
+      if (commandName === "createpoll") {
+        if (current_poll.length === 0) {
+          let poll_list = [];
+          let index = 0;
+
+          while (index < 10) {
+            const string = interaction.options.getString(`option_${index + 1}`);
+
+            if (string) {
+              poll_list = [...poll_list, { title: string, votes: [] }];
+            }
+            index++;
+          }
+
+          current_poll = poll_list;
+
+          interaction.reply("Poll Created");
+        } else {
+          interaction.reply(
+            "Please close the existing poll before starting a new one."
+          );
+        }
+      } else if (commandName === "closepoll") {
+        if (current_poll.length !== 0) {
+          let winner_total = 0;
+          let winner = "";
+
+          current_poll.forEach((item) => {
+            if (winner_total < item.votes.length) {
+              winner = item.title;
+            }
+          });
+
+          interaction.reply(`POLL CLOSED, WINNER: ${winner}`);
+
+          current_poll = [];
+        } else {
+          interaction.reply(
+            "Please create a poll before you try to close one."
+          );
+        }
+      } else if (commandName === "viewpoll") {
+        if (current_poll.length !== 0) {
+        } else {
+          interaction.reply("Please create a poll before you try to view one.");
+        }
+      }
+    } else {
+      interaction.reply(
+        "You lack required permissions to complete this command."
+      );
+    }
+
+    if (commandName === "vote") {
     }
   }
 });
