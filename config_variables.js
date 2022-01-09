9dfc9e669068efa2dfb634666b7c1ec9b68decb9
Daniel <danielcrewe1999@gmail.com>
2022-01-09 19:58:35 +0000

    first iteration completed, added mongodb integration for storing polls

--- config_variables.js
+++ config_variables.js
@@ -1,4 +1,7 @@
 export default {
+  MONGODB_URI: process.env.MONGODB_URI,
+  MONGODB_DB: process.env.MONGODB_DB,
+  MONGODB_COLLECTION: process.env.MONGODB_COLLECTION,
   DISCORD_TOKEN: process.env.TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
   GUILD_ID: process.env.GUILD_ID,
--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 // Required imports
 import { Client, Intents } from "discord.js";
 import CONFIG from "./config_variables.js";
+import { MongoClient } from "mongodb";
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+// Local Storage Of Current Running Poll
 let current_poll = [];
+
+// Connecting And Manipulating Data Saved To DB
+const DB_CONNECT = async (data, remove = false) => {
+  let client = await MongoClient.connect(CONFIG.MONGODB_URI);
+  let collection = client
+    .db(CONFIG.MONGODB_DB)
+    .collection(CONFIG.MONGODB_COLLECTION);
+
+  if (data && data.length > 0) {
+    if (remove === true) {
+      await collection.deleteOne({});
+    }
+
+    await collection.insertOne({ current_poll });
+  } else if (data && data.length === 0 && remove === true) {
+    await collection.deleteOne({});
+  }
+
+  let current_data = await collection.findOne({});
+
+  if (current_data && current_data.current_poll) {
+    current_poll = current_data.current_poll;
+  }
+};
 
 // On Ready
 client.once("ready", () => {
   console.log(`=== Logged In As: ${client.user.tag} ===`);
+  DB_CONNECT().then(() => {
+    console.log("=== Data Synced With Database ===");
+  });
 });
 
 // On Error
@@ -39,6 +68,8 @@ client.on("interactionCreate", async (interaction) => {
 
           current_poll = poll_list;
 
+          DB_CONNECT(current_poll);
+
           interaction.reply(
             `@everyone A New Poll Has Been Created:${poll_list.map(
               (item, index) => `\n${index + 1}. ${item.title}`
@@ -55,18 +86,21 @@ client.on("interactionCreate", async (interaction) => {
           let winner = "";
 
           current_poll.forEach((item) => {
-            if (winner_total < item.votes.length) {
+            if (item.votes.length > winner_total) {
               winner = item.title;
+              winner_total = item.votes.length;
             }
           });
 
           if (winner !== "") {
-            interaction.reply(`POLL CLOSED, WINNER: ${winner}`);
+            interaction.reply(`@everyone POLL CLOSED, WINNER: ${winner}`);
           } else {
-            interaction.reply(`POLL CLOSED, NO WINNER`);
+            interaction.reply(`@everyone POLL CLOSED, NO WINNER`);
           }
 
           current_poll = [];
+
+          DB_CONNECT([], true);
         } else {
           interaction.reply(
             "Please create a poll before you try to close one."
@@ -94,6 +128,76 @@ client.on("interactionCreate", async (interaction) => {
     }
 
     if (commandName === "vote") {
+      if (current_poll && current_poll.length > 0) {
+        let vote_list = [];
+        let vote_index = 0;
+
+        while (vote_index < 3) {
+          const string = interaction.options.getString(
+            `vote_${vote_index + 1}`
+          );
+
+          if (
+            string &&
+            parseFloat(string) > 0 &&
+            parseFloat(string) <= current_poll.length
+          ) {
+            vote_list = [...vote_list, string];
+          }
+          vote_index++;
+        }
+
+        if (vote_list.length > 0) {
+          let invalid_vote = false;
+
+          vote_list
+            .sort((a, b) => a - b)
+            .forEach((item, index) => {
+              if (vote_list[index - 1] && item === vote_list[index - 1]) {
+                invalid_vote = true;
+              }
+            });
+
+          if (invalid_vote === false) {
+            current_poll.forEach((item, index) => {
+              if (
+                item.votes &&
+                item.votes.findIndex(
+                  (item_two) => item_two === interaction.user.id.toString()
+                ) !== -1
+              ) {
+                let new_votes = item.votes;
+
+                new_votes.splice(
+                  item.votes.findIndex(
+                    (item_two) => item_two === interaction.user.id.toString()
+                  ),
+                  1
+                );
+
+                current_poll[index].votes = new_votes;
+              }
+            });
+
+            vote_list.forEach((item) => {
+              current_poll[item - 1].votes = [
+                ...current_poll[item - 1].votes,
+                interaction.user.id.toString(),
+              ];
+            });
+
+            DB_CONNECT(current_poll, true);
+
+            interaction.reply("Your votes have been submitted.");
+          } else {
+            interaction.reply("You cannot vote for the same option twice.");
+          }
+        } else {
+          interaction.reply("Error encountered while voting.");
+        }
+      } else {
+        interaction.reply("There is currently not a poll running.");
+      }
     }
   }
 });
